97243afe95a1a18267effaee71f8615e8a8d8429
NaserKh <naserK@360medlink.com>
2023-05-15 11:41:00 +0100

    issues dir vendor:fix

--- todolist/docker/nginx/index/index.php
+++ todolist/docker/nginx/index/index.php
@@ -1,7 +1,7 @@
 <?php
 define('LARAVEL_START', microtime(true));
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+require __DIR__. '../../../vendor/autoload.php';
+$app = require_once __DIR__. '../../../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
