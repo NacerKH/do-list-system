429eafc23781bd24c6ef77e7a05a7e3968894848
NaserKh <naserK@360medlink.com>
2023-05-17 02:54:47 +0100

    add routes && gitingore autogenerator

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContainerController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('tasks',TaskController::class);
+Route::apiResource('containers', ContainerController::class);
+Route::apiResource('tasks', TaskController::class);
+Route::patch('tasks/switchContainer/{id}', [TaskController::class, 'switch']);
